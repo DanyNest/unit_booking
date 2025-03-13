5f784dd5396affe071c079182d300bb132110e6b
DanyNest <nesterov1200997@gmail.com>
2025-03-13 20:16:24 +0200

    feature: remove redundant code

--- src/main/java/spribe/spribetechtask/model/dto/BookingOrderDto.java
+++ src/main/java/spribe/spribetechtask/model/dto/BookingOrderDto.java
@@ -1,8 +1,5 @@
 package spribe.spribetechtask.model.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.Data;
@@ -15,13 +12,7 @@ import spribe.spribetechtask.model.OrderStatus;
 public class BookingOrderDto implements Serializable {
   private long id;
   private LocalDateTime creationDate;
-
-  @Enumerated(EnumType.STRING)
   private OrderStatus status;
-
-  @Column(name = "booking_start_date")
   private LocalDateTime bookedFrom;
-
-  @Column(name = "booking_end_date")
   private LocalDateTime bookedTo;
 }
